2923c21a6c4fd76341831d704e9fb50e95511cb4
Rebecca Noy <rebecca.noy@gmx.de>
2024-02-01 16:41:12 +0100

    add token functions

--- src/utils/authentication.js
+++ src/utils/authentication.js
@@ -1,10 +1,26 @@
 const bcrypt = require('bcrypt')
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
+
+const secret = process.env.SECRET
 
 const hashPassword = async (plainPassword) => await bcrypt.hash(plainPassword, 12);
 
 const comparePassword = async (plainPassword, hashedPassword) => await bcrypt.compare(plainPassword, hashedPassword)
 
+const createToken = (payload, secret) => jwt.sign(payload, secret)
+
+const verifyToken = (token, secret) => {
+	try {
+		return jwt.verify(token, secret)
+	} catch (error) {
+		return false
+	}
+}
+
 module.exports = { 
 	hashPassword,
-	comparePassword
+	comparePassword,
+	createToken,
+	verifyToken
 	}
